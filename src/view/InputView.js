f808c269ce8980ad59482d61149ab10627c37e26
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-12-04 19:25:44 +0900

    feat: 사용자 입력값 받기

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,3 +1,18 @@
-const InputView = {};
+const { UTIL } = require("../Constant");
+const { Console } = require(UTIL);
+
+/**
+ * 사용자로부터 입력을 받는 역할을 한다.
+ */
+
+const InputView = {
+  readLine(askMessage, callback) {
+    Console.readLine(askMessage, callback);
+  },
+
+  readBaseballNumbers(callback) {
+    this.readLine("숫자를 입력해주세요", callback);
+  },
+};
 
 module.exports = InputView;
